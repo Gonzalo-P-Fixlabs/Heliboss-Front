acd5f9d365bed865beb19fe724c5b5e1ee3452df
Gonzalo-P-Fixlabs <gonzalop@fixlabs.cl>
2024-12-17 12:23:50 -0300

    Update login.ts

--- app/api/auth/login.ts
+++ app/api/auth/login.ts
@@ -22,6 +22,7 @@ export default async function Login(
         setCookie('auth-token', token, {
             // httpOnly: true,
             //secure: process.env.NODE_ENV === 'production',
+            secure: false,
             sameSite: 'strict',
             path: '/',
         });
